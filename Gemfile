c5e77f200e63e9122106aa50e19281e4a7c352cf
singhashish19031998@gmail.com <singhashish19031998@gmail.com>
2017-11-04 23:23:30 +0530

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -37,6 +37,10 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'faker', '~> 1.6', '>= 1.6.3'
+
+gem 'will_paginate', '~> 3.1', '>= 3.1.6'
+gem 'bootstrap-will_paginate', '~> 1.0'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -39,4 +39,21 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
+
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
+
+
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
+
 end
