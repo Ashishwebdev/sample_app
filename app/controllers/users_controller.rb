19b21c1c0ea1da37bbd73042e30e4b1f601be194
singhashish19031998@gmail.com <singhashish19031998@gmail.com>
2017-11-04 15:33:44 +0530

    authorization of one user is done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_action :log_in_user, only: [:edit,:update]
+  before_action :correct_user, only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -7,8 +10,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+
+  def edit
+    @user = User.find(params[:id])
+
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Your profile has been updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+
   def create
-    @user = User.new(params_new)
+    @user = User.new(user_params)
     if @user.save
       log_in(@user)
       flash[:successs] = "Welcome to Sample App"
@@ -19,12 +39,28 @@ class UsersController < ApplicationController
 
   end
 
+
+
+
   def User.new_token
     SecureRandom.urlsafe_base64
   end
 
   private
-  def params_new
+  def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
+  def log_in_user
+    unless logged_in
+      flash[:danger] = "Pleas log in first"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url unless current_user?(@user)
+  end
+
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,9 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s#{size}="
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+
+  def current_user? user
+    user == current_user
+  end
+
 end
